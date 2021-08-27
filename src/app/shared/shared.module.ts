b41f8e984700e319b7c8404f3882666c38277f47
jesus <jesus.alberto@desici.com>
2021-08-27 11:46:24 -0500

    Introducción al FormControl

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
@@ -15,7 +15,7 @@ import { MaterialModule } from '../material/material.module';
     ExponentialPipe,
     HighlightDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   exports: [
     ExponentialPipe,
@@ -27,6 +27,7 @@ import { MaterialModule } from '../material/material.module';
     CommonModule,
     RouterModule,
     MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class SharedModule { }
