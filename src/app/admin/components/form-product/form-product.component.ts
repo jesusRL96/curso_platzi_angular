f1ac38678ffc9bb27389a921d0858b4ab0118b70
jesus <jesus.alberto@desici.com>
2021-09-02 11:11:45 -0500

    Subiendo una imagen a Firebase Storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from '../../../core/services/products/products.service';
 import { myValidators } from '../../../utils/validators';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs/operators'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -11,15 +14,36 @@ import { myValidators } from '../../../utils/validators';
 })
 export class FormProductComponent implements OnInit {
   form: FormGroup;
+  image$: Observable<any>;
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
 
   ngOnInit(): void {
+  }
+
+  uploadFile(event){
+    const file = event.target.files[0];
+    console.log(file);
+    const filePath = 'images/nombre_imagen';
+    const ref = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    task.snapshotChanges()
+    .pipe(
+      finalize(()=>{
+        this.image$=ref.getDownloadURL()
+        this.image$.subscribe((url)=>{
+          console.log(url);
+          this.form.get('image').setValue(url)
+        });
+      })
+    )
+    .subscribe();
   }
 
   private buildForm() {
@@ -47,5 +71,6 @@ export class FormProductComponent implements OnInit {
   get priceField(){
     return this.form.get('price');
   }
+
 
 }
